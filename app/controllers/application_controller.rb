f6b23dd106a11a8420cdc7247c122792da53ffaf
cshupp <cshupp@gmail.com>
2019-03-13 14:34:15 -0400

    Adding in routes for javascript.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  include RouteHelper
+  before_action :setup_gon
+
+  def setup_gon
+    gon.routes = setup_routes
+    gon.packs = packed_assets
+  end
 end
---
+++ app/controllers/concerns/route_helper.rb
@@ -0,0 +1,84 @@
+module RouteHelper
+  include Webpacker::Helper
+  include ActionView::Helpers::AssetUrlHelper
+
+  IMAGE_EXTENSIONS = %w(.jpeg .jpg .png .gif).freeze
+  IMAGE_ROOT_PATH = 'packs/uts_dif/assets/images/'
+
+  def setup_routes
+    original_verbosity = $VERBOSE
+    $VERBOSE = nil
+    routes = Rails.application.routes.named_routes.helper_names - ['rails_blob_path', 'rails_blob_url', 'rails_representation_path', 'rails_representation_url']
+    $VERBOSE = original_verbosity
+    @@routes_hash ||= {}
+    if @@routes_hash.empty?
+      routes.each do |route|
+        begin
+          @@routes_hash[route] = self.send(route)
+        rescue ActionController::UrlGenerationError => ex
+          if (ex.message =~ /missing required keys: \[(.*?)\]/)
+            keys = $1
+            keys = keys.split(',')
+            keys.map! do |e|
+              e.gsub!(':', '')
+              e.strip
+            end
+            required_keys_hash = {}
+            keys.each do |key|
+              required_keys_hash[key.to_sym] = ':' + key.to_s
+            end
+            @@routes_hash[route] = self.send(route, required_keys_hash)
+          else
+            raise ex
+          end
+        end
+      end
+    end
+    #$log.debug('routes hash passed to javascript is ' + @@routes_hash.to_s)
+    @@routes_hash
+  end
+
+  def setup_packed_assets
+    if Webpacker.instance.config.cache_manifest?
+      @@packed_assets ||= packed_assets
+    else
+      @@packed_assets = packed_assets
+    end
+  end
+
+  private
+
+  def packed_assets
+    h = {}
+    h[:paths] = {}
+    h[:urls] = {}
+    h[:urls][:images] = {}
+    h[:paths][:images] = {}
+    Webpacker.instance.manifest.refresh.each_pair do |k,v|
+      unless k =~ /map$/
+        url = asset_pack_url k
+        path = asset_pack_path k
+        h[:urls][k] = url
+        h[:paths][k] = path
+        if IMAGE_EXTENSIONS.include?(File.extname k)
+          rootless = k.sub(IMAGE_ROOT_PATH,'')
+          h[:urls][:images][k] = url
+          h[:urls][:images][rootless] = url
+          h[:paths][:images][k] = path
+          h[:paths][:images][rootless] = path
+        end
+      end
+    end
+    h
+  end
+
+end
+=begin
+Webpacker.instance.manifest
+Webpacker.instance.manifest.refresh #gives hash
+include Webpacker::Helper
+include ActionView::Helpers::AssetUrlHelper
+include ActionView::Helpers::AssetTagHelper
+Webpacker.instance.config.cache_manifest?
+
+=end
