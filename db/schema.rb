414032f261cdbcf5f2c2dc656c71c5ab63c75cf8
Uzma Rahman <uzma.rahman@gmail.com>
2019-08-27 15:34:41 -0700

    added article model for failing test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_135636) do
+ActiveRecord::Schema.define(version: 2019_08_21_235857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "comment"
