ea0ab052effa444dacce56c89c86d6843b04d3eb
Uzma Rahman <uzma.rahman@gmail.com>
2019-08-30 09:11:08 -0700

    Add log info in schedule controller

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,10 +1,14 @@
 class ScheduleController < ApplicationController
   def get_facilities
-    render :json {data: [{id: 1, location: 'New York'}, {id: 2, location: 'Los Angeles'}, {id: 3, location: 'Portland'}]}
+    data = [{id: 1, location: 'New York'}, {id: 2, location: 'Los Angeles'}, {id: 3, location: 'Portland'}]
+    $log.info{"Get facilities is returning #{data.inspect}"}
+    render :json {data: data}
   end
 
   def get_appointment_types
-    render :json {data: [{id: 1, type: 'Allergist'}]}
+    appointment_types = [{id: 1, type: 'Allergist'}, {id: 2, type: 'Surgeon'}, {id: 3, type: 'Primary Care'}, {id:4, type: 'Cardiologist'}]
+    $log.info{"Get appointment types is returning #{appointment_types.inspect}"}
+    render :json {data: appointment_types}
   end
 
   def get_user
