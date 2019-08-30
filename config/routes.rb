6828d2e68e2459f483128cd39f058473e538d24b
cshupp <cshupp@gmail.com>
2019-08-30 12:47:32 -0400

    Updating controller back to Uzma's accidentally merged out code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   get 'get_facilities' => 'schedule#get_facilities'
   get 'get_appointment_types' => 'schedule#get_appointment_types'
-  get 'schedule/get_user'
-  # resources :comments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'fetch_time' => 'comments#fetch_time'
-  get 'list_comments' => 'comments#list_comments'
-  post 'add_comment' => 'comments#add_comment'
 
-  root :to => 'comments#root'
+  root :to => 'schedule#root'
 
 end
