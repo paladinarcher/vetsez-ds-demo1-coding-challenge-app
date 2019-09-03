c067aae1edc1b9531da88f6bf400128adf6b5d04
cshupp <cshupp@gmail.com>
2019-09-03 10:41:08 -0400

    Fixing image in accordance with this proposed solution from webpacker team: https://github.com/rails/webpacker/issues/1174

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+//https://github.com/rails/webpacker/issues/1174
+const config = require('@rails/webpacker/package/config')
+const ManifestPlugin = require('webpack-manifest-plugin')
+environment.plugins.append('Manifest', new ManifestPlugin({
+    publicPath: config.publicPath,
+    writeToFileEmit: true,
+    filter: f => {
+        f.name = f.name.replace(/media\/images\//g, 'media/packs/images/')
+      //  f.path = f.path.replace(/\\/g, '/')
+        return f
+    }
+}))
