3a4702276dbabec5cf9df8a77ccc4e9082c9345e
cshupp <cshupp@gmail.com>
2020-01-31 11:34:12 -0500

    Adding in VA GUI to build.

--- app/frontend/packs/application.jsx
+++ app/frontend/packs/application.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import axios from './utils/axios'
 import GH from './utils/gon_helper';
 import {Header, Header2, Main, Footer} from "./components/layout";
-
+import AlertBox from '@department-of-veterans-affairs/formation-react/AlertBox'
 // fix for IE11 allowing us to use axios/fetch for ajax calls
 import {promise, polyfill} from 'es6-promise';
 
@@ -33,7 +33,12 @@ export default class Application extends React.Component {
 
     render() {
         const cards = {
-            'welcome': <div>Welcome!</div>,
+            'welcome': <div className="usa-width-one-whole">
+                <AlertBox
+                    headline='Informational alert'
+                    content="YAY!!!!!!!    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam id felis pulvinar ligula ultricies sollicitudin eget nec dui. Cras augue velit, pellentesque sit amet nisl ut, tristique suscipit sem. Cras sollicitudin auctor mattis."
+                    status="info"
+                    isVisible/></div>,
         };
         return (
             <div style={{padding: '20px'}}>
