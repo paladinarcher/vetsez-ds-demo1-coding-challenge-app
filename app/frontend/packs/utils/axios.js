c79fabbd7ef6387c5d0a64d5bf48a99d5282fb5d
gpbowman-git <gpbowman.git@gmail.com>
2020-02-26 11:25:57 -0500

    added react-router and react-router-dom

--- app/frontend/packs/utils/axios.js
+++ app/frontend/packs/utils/axios.js
@@ -3,7 +3,8 @@
 //
 import axios from 'axios'
 
-const token = document.querySelector('[name="csrf-token"]') || {content: 'no-csrf-token'}
+const token = document.querySelector('[name="csrf-token"]') || {content: 'no-csrf-token'};
+console.log("token", token);
 const ax = axios.create({
     headers: {
         common: {
@@ -11,6 +12,16 @@ const ax = axios.create({
             'X-CSRF-Token': token.content
         }
     }
+});
+
+ax.interceptors.response.use(function (response) {
+    console.log("------------- axios response -----------------", response);
+    // Do something with response data
+    return response;
+}, function (error) {
+    console.log("------------- axios error -----------------", error);
+    // Do something with response error
+    return Promise.reject(error);
 });
 
 export default ax
