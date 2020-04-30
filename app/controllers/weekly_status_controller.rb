815aa200c933d6863b073b03d9665c9747c42f4c
cshupp <cshupp@gmail.com>
2020-04-30 14:31:16 -0400

    Adding upload to controller

--- app/controllers/weekly_status_controller.rb
+++ app/controllers/weekly_status_controller.rb
@@ -3,10 +3,10 @@ class WeeklyStatusController < ApplicationController
   end
 
   def upload
-    a = params
     if !params[ :file ].nil?
       ws = WeeklyStatus.new
-      ws.weekly_csv.attach(params[:file])
+      file = params[:file]
+      ws.weekly_csv.attach(io: file.tempfile, filename: file.original_filename )
       ws.user = current_user
       ws.save!
     end
--- app/models/weekly_status.rb
+++ app/models/weekly_status.rb
@@ -2,3 +2,10 @@ class WeeklyStatus < ApplicationRecord
   belongs_to :user
   has_one_attached :weekly_csv
 end
+=begin
+w = WeeklyStatus.all.last
+ a = w.weekly_csv
+ a.download
+a.content_type
+ a.download.split.first
+=end
