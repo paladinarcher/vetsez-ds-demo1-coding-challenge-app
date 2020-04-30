7dd39f3226b8117ce6b4dfdb6cad2ccad98680f3
cshupp <cshupp@gmail.com>
2020-04-30 16:32:52 -0400

    Checkpoint, parsing CSV.

--- app/controllers/weekly_status_controller.rb
+++ app/controllers/weekly_status_controller.rb
@@ -8,7 +8,12 @@ class WeeklyStatusController < ApplicationController
       file = params[:file]
       ws.weekly_csv.attach(io: file.tempfile, filename: file.original_filename )
       ws.user = current_user
-      ws.save!
+      WeeklyStatus.transaction do
+        ws.save!
+        ws.local_details.each do |detail|
+          detail.save!
+        end
+      end
     end
     redirect_to weekly_status_path
   end
--- app/models/weekly_status.rb
+++ app/models/weekly_status.rb
@@ -1,35 +1,36 @@
 require 'date'
+require 'csv'
 
 class WeeklyStatus < ApplicationRecord
   before_save :create_details
   belongs_to :user
   has_one_attached :weekly_csv
   has_many :weekly_status_details, dependent: :destroy
-
+  attr_accessor :local_details
   def create_details
-    csv = self.weekly_csv.download.split
-    headers = csv.shift
-    headers_map = {}
-    headers.split(',').each_with_index do |key, index|
-      headers_map[key] = index
-    end
+    @local_details = []
+    csv =  CSV.parse(self.weekly_csv.download,headers:true)
+    # headers = csv.headers
+    #     # headers_map = {}
+    #     # headers.split(',').each_with_index do |key, index|
+    #     #   headers_map[key] = index
+    #     # end
 
-    csv.each do |row|
+    csv.by_row.each do |row|
       wsd = WeeklyStatusDetail.new
-      wsd.send(:project_organization=,row[headers_map["ProjectOrganization"]])
-      wsd.send(:project_code=,row[headers_map["ProjectCode"]])
-      wsd.send(:project_title=,row[headers_map["ProjectTitle"]])
-      wsd.send(:task_number=,row[headers_map["TaskNumber"]])
-      wsd.send(:task=,row[headers_map["task"]].to_i)
-      wsd.send(:project_type=,row[headers_map["ProjectType"]])
-      wsd.send(:person=,row[headers_map["Person"]])
-      parsed_date = Date.strptime(row[headers_map["Date"]], '%m/%d/%Y')
+      wsd.send(:project_organization=,row["ProjectOrganization"])
+      wsd.send(:project_code=,row["ProjectCode"])
+      wsd.send(:project_title=,row["ProjectTitle"])
+      wsd.send(:task_number=,row["TaskNumber"])
+      wsd.send(:task=,row["Task"].to_i)
+      wsd.send(:project_type=,row["ProjectType"])
+      wsd.send(:person=,row["Person"])
+      parsed_date = Date.strptime(row["Date"], '%m/%d/%Y')
       wsd.send(:task_date=,parsed_date)
-      wsd.send(:comments=,row[headers_map["Comments"]])
-      wsd.send(:hours=,row[headers_map["Hours"].to_f])
+      wsd.send(:comments=,row["Comments"])
+      wsd.send(:hours=,row["Hours"].to_f)
       wsd.weekly_status = self
-      # wsd.save!
-      # self.weekly_status_details << wsd
+      @local_details << wsd
     end
   end
 end
